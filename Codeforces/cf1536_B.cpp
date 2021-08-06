89cc6f8d85265025319f945df402cc67f2cae385
aerou <houhin0809@gmail.com>
2021-08-07 05:03:24 +0800

    2021/8/7

---
+++ Codeforces/cf1536_B.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char a[1000];
+char p[1010];
+int n;
+
+bool check(const char arr[], const char patt[], int n_p, int m)
+{
+	bool flag = true;
+	int nxt[1010];
+	memset(nxt, 0, sizeof(nxt));
+
+	for (int i = 2, j = 0; i <= n_p; i++)
+	{
+		while (j && patt[i] != patt[j + 1]) j = nxt[j];
+		if (patt[i] == patt[j + 1]) j++;
+		nxt[i] = j;
+	}
+
+	for (int i = 1, j = 0; i <= m; i++)
+	{
+		while (j && arr[i] != patt[j + 1]) j = nxt[j];
+		if (arr[i] == patt[j + 1]) j++;
+		if (j == n_p)
+		{
+			flag = false;
+			j = nxt[j];
+		}
+	}
+
+	return flag;
+}
+
+char dfs_p[1010];
+bool flag = true;
+void dfs(int s, int limit)
+{
+	if (!flag) return;
+	if (s > limit)
+	{
+		if (check(a, dfs_p, limit, n))
+		{
+			for (int i = 1; i <= limit; i++) cout << dfs_p[i];
+			cout << '\n';
+			flag = false;
+		}
+		return;
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		dfs_p[s] = (char)(i + 'a');
+		dfs(s + 1, limit);
+		if (!flag) return;
+	}
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> n;
+		flag = true;
+
+		for (int i = 1; i <= n; i++) cin >> a[i];
+
+		for (int len = 1; len <= 1000; len++)
+		{
+			dfs(1, len);
+			if (!flag) break;
+		}
+	}
+
+	return 0;
+}
--- Codeforces/cf545_E.cpp
+++ Codeforces/cf545_E.cpp
@@ -5,125 +5,125 @@
 
 using namespace std;
 
-const long long INF=0x3f3f3f3f3f3f3f3f;
-const int maxn=3e5+5;
-const int maxm=6e5+5;
+const long long INF = 0x3f3f3f3f3f3f3f3f;
+const int maxn = 3e5 + 5;
+const int maxm = 6e5 + 5;
 
-long long dis[maxn],ans[maxn];
+long long dis[maxn], ans[maxn];
 bool vis[maxn];
 int pre[maxn];
 long long sum;
 
 typedef pair<long long, int> PLLI;
 
-int n,m,s;
+int n, m, s;
 
-struct edge{
-    int to,next;
-    long long val;
-}e[maxm];
-int head[maxn],tot=0;
+struct edge {
+	int to, next;
+	long long val;
+} e[maxm];
+int head[maxn], tot = 0;
 
 void add_edge(int from, int to, long long val)
 {
-    e[++tot].to=to;
-    e[tot].val=val;
-    e[tot].next=head[from];
-    head[from]=tot;
+	e[++tot].to = to;
+	e[tot].val = val;
+	e[tot].next = head[from];
+	head[from] = tot;
 }
 
 inline int read()
 {
-    int x=0,f=1;
-    char c=getchar();
-    while(c<'0' || c>'9')
-    {
-        if(c=='-')
-        {
-            f=-1;
-        }
-        c=getchar();
-    }
-    while(c <= '9' && c >= '0')
-    {
-        x = (x<<1)+(x<<3)+(c^48),c=getchar();
-    }
-    return x*f;
+	int x = 0, f = 1;
+	char c = getchar();
+	while (c < '0' || c > '9')
+	{
+		if (c == '-')
+		{
+			f = -1;
+		}
+		c = getchar();
+	}
+	while (c <= '9' && c >= '0')
+	{
+		x = (x << 1) + (x << 3) + (c ^ 48), c = getchar();
+	}
+	return x * f;
 }
 
 void dijkstra(int st)
 {
-    priority_queue<PLLI, vector<PLLI>, greater<PLLI>> q;
+	priority_queue<PLLI, vector<PLLI>, greater<PLLI>> q;
 
-    for (int i=1;i<=n;i++)
-    {
-        dis[i]=INF;
-        vis[i]=false;
-    }
-    dis[st]=0;
-    q.push({0,st});
+	for (int i = 1; i <= n; i++)
+	{
+		dis[i] = INF;
+		vis[i] = false;
+	}
+	dis[st] = 0;
+	q.push({0, st});
 
-    while (!q.empty())
-    {
-        auto u=q.top();
-        q.pop();
+	while (!q.empty())
+	{
+		auto u = q.top();
+		q.pop();
 
-        if (vis[u.second]) continue;
+		if (vis[u.second]) continue;
 
-        vis[u.second]=true;
+		vis[u.second] = true;
 
-        for (int i=head[u.second];i;i=e[i].next)
-        {
-            int j=e[i].to;
-            long long val=e[i].val;
+		for (int i = head[u.second]; i; i = e[i].next)
+		{
+			int j = e[i].to;
+			long long val = e[i].val;
 
-            if (dis[j]>dis[u.second]+val)
-            {
-                dis[j]=dis[u.second]+val;
-                q.push({dis[j],j});
-                pre[j]=i;
-            }
+			if (dis[j] > dis[u.second] + val)
+			{
+				dis[j] = dis[u.second] + val;
+				q.push({dis[j], j});
+				pre[j] = i;
+			}
 
-            if (dis[j]==dis[u.second]+val && val<e[pre[j]].val) pre[j]=i;
-        }
-    }
+			if (dis[j] == dis[u.second] + val && val < e[pre[j]].val) pre[j] = i;
+		}
+	}
 }
 
 int main()
 {
-    n=read(),m=read();
+	n = read(), m = read();
 
-    for (int i=1;i<=m;i++)
-    {
-        int f=read(),t=read();
-        long long v=read();
-        add_edge(f,t,v),add_edge(t,f,v);
-    }
+	for (int i = 1; i <= m; i++)
+	{
+		int f = read(), t = read();
+		long long v = read();
+		add_edge(f, t, v), add_edge(t, f, v);
+	}
 
-    s=read();
+	s = read();
 
-    dijkstra(s);
+	dijkstra(s);
 
-    int cnt=0;
-    for (int i=1;i<=n;i++)
-    {
-        if (i==s) continue;
-        int id=pre[i];
-        long long val=e[id].val;
-        sum+=val;
-        ans[++cnt]=id;
-    }
+	int cnt = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i == s) continue;
+		int id = pre[i];
+		long long val = e[id].val;
+		sum += val;
+		ans[++cnt] = id;
+	}
 
-    sort(ans+1,ans+cnt+1);
+	sort(ans + 1, ans + cnt + 1);
 
-    printf("%lld\n",sum);
+	printf("%lld\n", sum);
 
-    for (int i=1;i<=cnt;i++)
-    {
-        printf("%lld ",(ans[i]+1)/2);
-    }
+	for (int i = 1; i <= cnt; i++)
+	{
+		printf("%lld ", (ans[i] + 1) / 2);
+	}
 
-    return 0;
+	return 0;
 }
 
 
