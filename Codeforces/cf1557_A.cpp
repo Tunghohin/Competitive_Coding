9db05a758bdd9d7ade93645b7015f0d1e6817b0a
aerou <houhin0809@gmail.com>
2021-08-10 02:22:48 +0800

    2021/8/10

--- Codeforces/cf1557_A.cpp
+++ Codeforces/cf1557_A.cpp
@@ -1,4 +1,34 @@
-//
-// Created by admin on 2021/8/10.
-//
+#include <iostream>
+#include <cstring>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n, maxn = -1e9;
+		long long sum = 0;
+		cin >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			int x;
+			cin >> x;
+			sum += x;
+			maxn = max(maxn, x);
+		}
+
+		sum -= maxn;
+		cout << fixed << setprecision(10) << maxn + static_cast<double>(sum) / (n - 1) << '\n';
+	}
+
+	return 0;
+}
