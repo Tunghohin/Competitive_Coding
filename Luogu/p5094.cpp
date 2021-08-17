555a4c5c742745c8ee36040f6b5043f4317fff43
aerou <houhin0809@gmail.com>
2021-08-17 12:52:27 +0800

    2021/8/17

--- Luogu/p5094.cpp
+++ Luogu/p5094.cpp
@@ -1,4 +1,14 @@
-//
-// Created by admin on 2021/8/17.
-//
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	
+}
