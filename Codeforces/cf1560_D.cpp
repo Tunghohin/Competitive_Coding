4b073ccc3f211b03a66fad67d558f22929013f13
aerou <houhin0809@gmail.com>
2021-08-19 23:31:37 +0800

    2021/8/19

---
+++ Codeforces/cf1560_D.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+
+const LL Pow2Lim = 2e18;
+
+vector<string> P2;
+
+int check(const string &p, const string &s)
+{
+	int sp = 0;
+	int pp = 0;
+	int cnt = 0;
+
+	while (pp < p.length() && sp < s.length())
+	{
+		if (p[pp] == s[sp])
+		{
+			cnt++;
+			sp++;
+		}
+		pp++;
+	}
+
+	return (int)p.length() - cnt + (int)s.length() - cnt;
+}
+
+int main()
+{
+	for (LL Pow2 = 1; Pow2 <= Pow2Lim; Pow2 *= 2)
+	{
+		P2.push_back(to_string(Pow2));
+	}
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		string s;
+		cin >> s;
+
+		int ans = 2 * (int)s.length();
+
+		for (const auto &i : P2)
+		{
+			ans = min(ans, check(s, i));
+		}
+
+		cout << ans << '\n';
+	}
+}
