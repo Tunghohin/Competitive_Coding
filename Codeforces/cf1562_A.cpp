231a7720a05cb725ae2d1baa155f46491cd7aa9d
aerou <houhin0809@gmail.com>
2021-08-27 13:24:31 +0800

    2021/8/27

--- Codeforces/cf1562_A.cpp
+++ Codeforces/cf1562_A.cpp
@@ -4,16 +4,75 @@ using namespace std;
 
 void solve()
 {
-	int l, r;
-	cin >> l >> r;
-
-	if (l > (r / 2) + 1)
+	int len;
+	string s;
+	cin >> len >> s;
+	bool flag = true;
+	for (int i = 0; i < len; i++)
 	{
-		cout << r % l << '\n';
+		if (((s[i] - '0') % 2 == 0 && s[i] - '0' != 2) || s[i] - '0' == 1 || s[i] - '0' == 9)
+		{
+			cout << 1 << '\n';
+			cout << s[i] - '0' << '\n';
+			flag = false;
+			break;
+		}
 	}
-	else
+
+	for (int i = 0; i < len - 1 && flag; i++)
 	{
-		cout << r % ((r/2) + 1) << '\n';
+		if (s[i] - '0' == 2 && flag)
+		{
+			for (int j = i + 1; j < len; j++)
+			{
+				if (s[j] - '0' == 2 || s[j] - '0' == 5 || s[j] - '0' == 7)
+				{
+					cout << 2 << '\n';
+					cout << s[i] - '0' << s[j] - '0' << '\n';
+					flag = false;
+					break;
+				}
+			}
+		}
+		if (s[i] - '0' == 3 && flag)
+		{
+			for (int j = i + 1; j < len; j++)
+			{
+				if (s[j] - '0' == 5 || s[j] - '0' == 2 || s[j] - '0' == 3)
+				{
+					cout << 2 << '\n';
+					cout << s[i] - '0' << s[j] - '0' << '\n';
+					flag = false;
+					break;
+				}
+			}
+		}
+		else if (s[i] - '0' == 5 && flag)
+		{
+			for (int j = i + 1; j < len; j++)
+			{
+				if (s[j] - '0' == 5 || s[j] - '0' == 2 ||  s[j] - '0' == 7)
+				{
+					cout << 2 << '\n';
+					cout << s[i] - '0' << s[j] - '0' << '\n';
+					flag = false;
+					break;
+				}
+			}
+		}
+		else if (s[i] - '0' == 7 && flag)
+		{
+			for (int j = i + 1; j < len; j++)
+			{
+				if (s[j] - '0' == 7 || s[j] - '0' == 2 || s[j] - '0' == 5)
+				{
+					cout << 2 << '\n';
+					cout << s[i] - '0' << s[j] - '0' << '\n';
+					flag = false;
+					break;
+				}
+			}
+		}
 	}
 }
 
@@ -29,5 +88,4 @@ int main()
 	{
 		solve();
 	}
-
 }
