b32e939606ef44b303fa09e5bcfb07cf6588cfb9
aerou <houhin0809@gmail.com>
2021-09-10 19:40:48 +0800

    2021/9/10

--- Luogu/p3128.cpp
+++ Luogu/p3128.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+int d[50010];
+
 struct edge
 {
 	int to, next;
@@ -18,11 +20,71 @@ void add_edge(int from, int to)
 }
 
 int depth[50010];
-int f[50010][15];
+int f[50010][20];
 void bfs(int root)
 {
 	queue<int> q;
 	memset(depth, 0x3f, sizeof(depth));
+
+	depth[0] = 0, depth[root] = 1;
+
+	q.push(root);
+
+	while (!q.empty())
+	{
+		int u = q.front();
+		q.pop();
+
+		for (int i = head[u]; i; i = e[i].next)
+		{
+			int j = e[i].to;
+
+			if (depth[j] > depth[u] + 1)
+			{
+				depth[j] = depth[u] + 1;
+				q.push(j);
+
+				f[j][0] = u;
+				for (int k = 1; k <= 15; k++) f[j][k] = f[f[j][k - 1]][k - 1];
+			}
+		}
+	}
+}
+
+int lca(int a, int b)
+{
+	if (depth[a] < depth[b]) return lca(b, a);
+
+	for (int k = 15; k >= 0; k--)
+	{
+		if (depth[f[a][k]] >= depth[b]) a = f[a][k];
+	}
+
+	if (a == b) return b;
+
+	for (int k = 15; k >= 0; k--)
+	{
+		if (f[a][k] != f[b][k]) a = f[a][k], b = f[b][k];
+	}
+
+	return f[b][0];
+}
+
+int maxn = -0x3f3f3f3f;
+int dfs(int u, int last)
+{
+	int now = d[u];
+
+	for (int i = head[u]; i; i = e[i].next)
+	{
+		int j = e[i].to;
+		if (j == last) continue;
+
+		now += dfs(j, u);
+	}
+
+	maxn = max(maxn, now);
+	return now;
 }
 
 int main()
@@ -39,4 +101,21 @@ int main()
 		cin >> a >> b;
 		add_edge(a, b), add_edge(b, a);
 	}
+
+	bfs(1);
+
+	while (k--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		int p = lca(a, b);
+		d[a]++, d[b]++;
+		d[p]--;
+		d[f[p][0]]--;
+	}
+
+	dfs(1, 0);
+
+	cout << maxn << '\n';
 }
