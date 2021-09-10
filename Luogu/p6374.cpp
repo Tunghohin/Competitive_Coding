31b181b78b41946ec18a1b2e19819267da3b014f
aerou <houhin0809@gmail.com>
2021-09-10 15:22:11 +0800

    2021/9/10

--- Luogu/p6374.cpp
+++ Luogu/p6374.cpp
@@ -1,117 +1,122 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
+
 using namespace std;
-int n, q, idx, t;
-int head[500010], dep[500010], size[500010], f[500010][25];
-struct node {
-	int nxt, to;
-} edge[1000010];
-void add(int u, int v)
+
+int depth[500010];
+int f[500010][25];
+int size[500010];
+
+struct edge
 {
-	edge[++idx].nxt = head[u];
-	edge[idx].to = v;
-	head[u] = idx;
-}
-void dfs(int now, int fath)
+	int to, next;
+}e[1000010];
+int head[500010], tot = 0;
+
+void add_edge(int from, int to)
 {
-	dep[now] = dep[fath] + 1;
-	size[now] = 1;
-	f[now][0] = fath;
-	for (int i = 1; i <= t; i++)
-	{
-		f[now][i] = f[f[now][i - 1]][i - 1];
-	}
-	for (int i = head[now]; i; i = edge[i].nxt)
-	{
-		int v = edge[i].to;
-		if (v == fath)
-			continue;
-		dfs(v, now);
-		size[now] += size[v];
-	}
+	e[++tot].to = to;
+	e[tot].next = head[from];
+	head[from] = tot;
 }
-int LCA(int x, int y)
+
+void dfs(int u, int last)
 {
-	if (x == y)
-		return x;
-	if (dep[x] < dep[y])
-		swap(x, y);
-	for (int i = t; i >= 0; i--)
+	depth[u] = depth[last] + 1;
+	size[u] = 1;
+
+	f[u][0] = last;
+	for (int i = 1; i <= 15; i++) f[u][i] = f[f[u][i - 1]][i - 1];
+
+	for (int i = head[u]; i; i = e[i].next)
 	{
-		if (dep[f[x][i]] >= dep[y])
-		{
-			x = f[x][i];
-			if (x == y)
-				return x;
-		}
+		int j = e[i].to;
+
+		if (j == last) continue;
+
+		dfs(j, u);
+		size[u] += size[j];
 	}
-	for (int i = t; i >= 0; i--)
-	{
-		if (f[x][i] != f[y][i])
-		{
-			x = f[x][i];
-			y = f[y][i];
-		}
-	}
-	return f[x][0];
 }
-int get(int x, int y)//查询x所在的子树的大小，根为y，不包含y这个点
+
+int lca(int a, int b)
 {
-	if (x == y)
-		return 0;
-	for (int i = t; i >= 0; i--)
+	if (a == b) return b;
+	if (depth[a] < depth[b]) swap(a, b);
+
+	for (int k = 15; k >= 0; k--)
 	{
-		if (dep[f[x][i]] > dep[y])
-		{
-			x = f[x][i];
-		}
+		if (depth[f[a][k]] >= depth[b]) a = f[a][k];
+		if (a == b) return b;
 	}
-	return size[x];
+
+	for (int k = 15; k >= 0; k--)
+	{
+		if (f[a][k] != f[b][k]) a = f[a][k], b = f[b][k];
+	}
+
+	return f[b][0];
 }
-int dis(int x, int y)//求树上两点间距离
+
+int get(int a, int b)
 {
-	return dep[x] + dep[y] - 2 * dep[LCA(x, y)];
+	if (a == b) return 0;
+
+	for (int k = 15; k >= 0; k--)
+	{
+		if (depth[f[a][k]] > depth[b]) a = f[a][k];
+	}
+
+	return size[a];
 }
+
+int get_dist(int a, int b)
+{
+	return depth[a] + depth[b] - (2 * depth[lca(a, b)]);
+}
+
 int main()
 {
-	cin >> n >> q;
-	t = (int)(log(n) / log(2)) + 1;
+	int n, q;
+	scanf("%d%d", &n, &q);
+
 	for (int i = 1; i <= n - 1; i++)
 	{
-		int u, v;
-		scanf("%d%d", &u, &v);
-		add(u, v);
-		add(v, u);
+		int fr, to;
+		scanf("%d%d", &fr, &to);
+		add_edge(fr, to), add_edge(to, fr);
 	}
+
 	dfs(1, 0);
-	while (q--)
+
+	for (int i = 1; i <= q; i++)
 	{
-		int a, b, c, ans = 0;
+		int a, b, c;
 		scanf("%d%d%d", &a, &b, &c);
-		int lca = LCA(a, b);
-		//无解情况，c不在a->b的路径上
-		if (lca == c)//如果c就等于真实的lca,ans=n-size[ra]-size[rb]
+
+		int p = lca(a, b);
+
+		if (c == p)
 		{
-			ans = n - get(a, c) - get(b, c);
-			printf("%d\n", ans);
+			int res =  n - get(a, c) - get(b, c);
+			printf("%d\n", res);
 			continue;
 		}
-		//c在a->lca(a,b)的路径上时，ans=size[c]-size[ra]
-		//判断c在不在a->lca路径上：dis(a,c)+dis(c,lca)==dis(a,lca) ?
-		//写成函数吧，不然太麻烦了
-		if (dis(a, c) + dis(c, lca) == dis(a, lca))
+
+		if (get_dist(a, c) + get_dist(c, p) == get_dist(a, p))
 		{
-			ans = size[c] - get(a, c);
-			printf("%d\n", ans);
+			int res = size[c] - get(a, c);
+			printf("%d\n", res);
 			continue;
 		}
-		//c在b->lca(b,c)的路径上时，ans=size[c]-size[rb]
-		if (dis(b, c) + dis(c, lca) == dis(b, lca))
+
+		if (get_dist(b, c) + get_dist(c, p) == get_dist(b, p))
 		{
-			ans = size[c] - get(b, c);
-			printf("%d\n", ans);
+			int res = size[c] - get(b, c);
+			printf("%d\n", res);
 			continue;
 		}
+
 		printf("0\n");
 	}
-	return 0;
 }
