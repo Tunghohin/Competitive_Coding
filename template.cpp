40dfb5ea41289a093f04002a30a65f0e6a8c4f44
aerou <houhin0809@gmail.com>
2021-10-02 16:19:42 +0800

    2021/10/2

---
+++ template.cpp
@@ -0,0 +1,10 @@
+/*
+inline int read()
+{
+    int s = 0, w = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') { if (ch == '-')w = -1; ch = getchar(); }
+    while (ch >= '0' && ch <= '9') s = s * 10 + ch - '0', ch = getchar();
+    return s * w;
+}
+*/
