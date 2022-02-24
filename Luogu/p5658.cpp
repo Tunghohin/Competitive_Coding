cc749306b1fde90373385ac2a04a01873377fccb
aerou <houhin0809@gmail.com>
2022-02-24 20:18:36 +0800

    2021/11/19

---
+++ Luogu/p5658.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int f[500010];
+int need[500010];
+int matched[500010];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int n;
+	string s;
+	cin >> n;
+	cin >> s;
+
+	for (int i = 2; i <= n; i++) cin >> f[i];
+
+	int res = 0;
+	
+}
